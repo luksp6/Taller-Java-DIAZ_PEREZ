20aadc36b59b8023995f1337f24e6986ef10f1fc
Lucas Pérez <lucasperez2001@gmail.com>
2025-05-23 20:04:07 -0300

    entrega

--- src/main/java/ejercicios/Ejercicio1.java
+++ src/main/java/ejercicios/Ejercicio1.java
@@ -62,57 +62,119 @@ public class Ejercicio1 extends Ejercicio
     public void test()
     {
         String cadena = "pablito clavo un clavito, que clavito clavo pablito pablito,que,que,que;que%que,que";
+        int delimitador = 4;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
         System.out.println("Resultado esperado: pablito");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, 4));
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
         System.out.println();
 
-        
+        cadena = "";
+        delimitador = 1;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);
         System.out.println("Resultado esperado: ");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("", 1));
-        System.out.println();
-        
-        System.out.println("Resultado esperado: ");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada(",,,;;;!!!", 1));
-        System.out.println();
-       
-        System.out.println("Resultado esperado: Hola");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("Hola", 1));
-        System.out.println();
-        
-        System.out.println("Resultado esperado: Hola");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("hola hola mundo mundo hola", 4));
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
         System.out.println();
 
+        cadena = ",,,;;;!!!";
+        delimitador = 1;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);        
         System.out.println("Resultado esperado: ");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("a bb ccc dddd", 5));
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
         System.out.println();
-            
+
+        cadena = "Hola";
+        delimitador = 1;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);       
+        System.out.println("Resultado esperado: Hola");
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
+        System.out.println();
+
+        cadena = "hola hola mundo mundo hola";
+        delimitador = 4;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);        
+        System.out.println("Resultado esperado: Hola");
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
+        System.out.println();
+
+        cadena = "a bb ccc dddd";
+        delimitador = 5;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);
+        System.out.println("Resultado esperado: ");
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
+        System.out.println();
+
+        cadena = "apple banana apple banana pear";
+        delimitador = 5;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);            
         System.out.println("Resultado esperado: apple");        
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("apple banana apple banana pear", 5));
-        System.out.println();
-        
-        System.out.println("Resultado esperado: Hola");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("Hola123 456Hola adios! hola_hola", 4));
-        System.out.println();
-        
-        System.out.println("Resultado esperado: Supercalifragilisticoespialidoso");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("Supercalifragilisticoespialidoso otra supercalifragilisticoespialidoso", 10));
-        System.out.println();
-        
-        System.out.println("Resultado esperado: Hola");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("Hola hola HOLA", 4));
-        System.out.println();
-        
-        System.out.println("Resultado esperado: ");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("perro gato elefante", 20));
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
         System.out.println();
 
+        cadena = "Hola123 456Hola adios! hola_hola";
+        delimitador = 4;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);        
         System.out.println("Resultado esperado: Hola");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("Hola hOla hoLa holA HOLA hola chau chau", 4));
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
         System.out.println();
-        
+
+        cadena = "Supercalifragilisticoespialidoso otra supercalifragilisticoespialidoso";
+        delimitador = 10;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);        
+        System.out.println("Resultado esperado: Supercalifragilisticoespialidoso");
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
+        System.out.println();
+
+        cadena = "Hola hola HOLA";
+        delimitador = 4;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);        
+        System.out.println("Resultado esperado: Hola");
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
+        System.out.println();
+
+        cadena = "perro gato elefante";
+        delimitador = 20;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);        
+        System.out.println("Resultado esperado: ");
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
+        System.out.println();
+
+        cadena = "Hola hOla hoLa holA HOLA hola chau chau";
+        delimitador = 4;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);
+        System.out.println("Resultado esperado: Hola");
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
+        System.out.println();
+
+        cadena = "a".repeat(100);
+        delimitador = 99;
+        System.out.println("Entrada: ");
+        System.out.println(cadena);
+        System.out.println("Delimitador: " + delimitador);        
         System.out.println("Resultado esperado: aaaaaaaaaa... (100 'a's)");
-        System.out.println("Resultado obtenido: " + this.palabraMasUsada("a".repeat(100), 99));
+        System.out.println("Resultado obtenido: " + this.palabraMasUsada(cadena, delimitador));
         System.out.println();
     }
 }
